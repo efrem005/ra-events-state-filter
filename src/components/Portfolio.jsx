3620c412371e49a7367709607accad7e1a23cd07
Nikolay Efremov <efrem_nn@mail.ru>
2026-03-04 19:15:51 +0700

    Events state filter

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,125 @@
+import { Component } from 'react';
+import Toolbar from './Toolbar';
+import ProjectList from './ProjectList';
+
+// Данные
+const projectsData = [
+  {
+    img: "/img/mon.jpg",
+    category: "Business Cards"
+  }, 
+  {
+    img: "/img/200.jpg",
+    category: "Websites"
+  }, 
+  {
+    img: "/img/emi_haze.jpg",
+    category: "Websites"
+  }, 
+  {
+    img: "/img/codystretch.jpg",
+    category: "Websites"
+  }, 
+  {
+    img: "/img/Triangle_003.jpg",
+    category: "Business Cards"
+  }, 
+  {
+    img: "/img/place200x290.png",
+    category: "Websites"
+  }, 
+  {
+    img: "/img/200.jpg",
+    category: "Websites"
+  }, 
+  {
+    img: "/img/transmission.jpg",
+    category: "Business Cards"
+  }, 
+  {
+    img: "/img/place200x290_1.png",
+    category: "Websites"
+  }, 
+  {
+    img: "/img/place200x290_2.png",
+    category: "Flayers"
+  }, 
+  {
+    img: "/img/the_ninetys_brand.jpg",
+    category: "Websites"
+  }, 
+  {
+    img: "/img/dia.jpg",
+    category: "Business Cards"
+  }, 
+  {
+    img: "/img/Triangle_350x197.jpg",
+    category: "Websites"
+  }, 
+  {
+    img: "/img/emi_haze.jpg",
+    category: "Websites"
+  }, 
+  {
+    img: "/img/transmission.jpg",
+    category: "Business Cards"
+  }, 
+  {
+    img: "/img/Triangle_350x197_1.jpg",
+    category: "Websites"
+  }, 
+  {
+    img: "/img/place200x290_3.png",
+    category: "Flayers"
+  }
+];
+
+class Portfolio extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      selectedFilter: 'All'
+    };
+    // Привязываем обработчик к контексту
+    this.handleSelectFilter = this.handleSelectFilter.bind(this);
+  }
+
+  handleSelectFilter(filter) {
+    this.setState({ selectedFilter: filter });
+  }
+
+  getFilteredProjects() {
+    const { selectedFilter } = this.state;
+    
+    if (selectedFilter === 'All') {
+      return projectsData;
+    }
+    
+    return projectsData.filter(project => project.category === selectedFilter);
+  }
+
+  getUniqueCategories(projects) {
+    const categories = projects.map(project => project.category);
+    const uniqueCategories = ['All', ...new Set(categories)];
+    return uniqueCategories;
+  };
+
+  render() {
+    const { selectedFilter } = this.state;
+    const filters = this.getUniqueCategories(projectsData);
+    const filteredProjects = this.getFilteredProjects();
+
+    return (
+      <div className="portfolio">
+        <Toolbar 
+          filters={filters}
+          selected={selectedFilter}
+          onSelectFilter={this.handleSelectFilter}
+        />
+        <ProjectList projects={filteredProjects} />
+      </div>
+    );
+  }
+}
+
+export default Portfolio;
---
+++ src/components/Toolbar.jsx
@@ -0,0 +1,17 @@
+function Toolbar({ filters, selected, onSelectFilter }) {
+  return (
+    <div className="toolbar">
+      {filters.map((filter) => (
+        <button
+          key={filter}
+          className={`toolbar-btn ${selected === filter ? 'active' : ''}`}
+          onClick={() => onSelectFilter(filter)}
+        >
+          {filter}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Toolbar;
